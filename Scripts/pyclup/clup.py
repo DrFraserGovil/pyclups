b120f797e01785f9256b12d07ebc807a4c27f2ed
Jack <jf20@sanger.ac.uk>
2024-02-21 15:58:13 +0000

    Preparing for merge with main: clup.py sanitised

--- Scripts/pyclup/clup.py
+++ Scripts/pyclup/clup.py
@@ -10,7 +10,7 @@ class CLUP:
 		self.Constraints = constraint
 		self.Basis = basis
 
-	def Predict(self,predictPoints,dataT,dataX,errorX=1e-20,getErrors=False):
+	def Predict(self,predictPoints,dataT,dataX,errorX=1e-20):
 		self.Constraints.Validate(predictPoints)
 
 		self._InitialiseComponents(predictPoints,dataT,dataX,errorX)
@@ -30,13 +30,7 @@ class CLUP:
 			for i in range(len(predictPoints)):
 				ai = self.a_blups[i] + corrector[i]/self.beta * self.delta
 				self.p_clups[i] = ai.T@dataX
-		print(self.p_clups.T)
-		e = None
-		if getErrors:
-			e=self._Errors()
-		pred = pyclup.Prediction(predictPoints,self.p_clups,0,self.p_blups,self.p_blps,e)
-		pred.blpE = self.blups_error.reshape((len(self.blups_error,)))
-		return pred
+		return pyclup.Prediction(predictPoints,self.p_clups,0,self.p_blups,self.p_blps)
 
 	def _InitialiseComponents(self,predictPoints,dataT,dataX,errorX):
 		eX = np.array(errorX)
@@ -91,32 +85,25 @@ class CLUP:
 
 			self.p_blps[i] = dx.T@self.a_blps[i]
 			self.p_blups[i] = dx.T@self.a_blups[i]
-			self.blups_error[i] = self.Kernel(predictPoints[i],predictPoints[i]) + ai.T@self.K@ai - 2 * self.ks[i].T@ai
-
 
 		self._X_data = dx
 		self.Dpblub = self.Constraints.Matrix() @ self.p_blups
-		self.MSE_Offset = 0
-		for i in range(len(dataT)):
-			self.MSE_Offset += self.Kernel(dataT[i],dataT[i])
-		
+				
 	def _Optimise(self, predictPoints):
 
 
 		#find a good initial position
-		
 		self.Constraints.InitialPosition(self.p_blups)
-		# return
-		# return
-		# if self.Constraints.c.Revertible:
-		# 	cT = self.Dpblub
-		# 	self.Constraints.c.Invert(cT)
+
+		#initialise optimiser parameters
 		ms = np.zeros(shape=np.shape(self.Constraints.TransformDimension,))
 		vs = np.zeros(shape=np.shape(self.Constraints.TransformDimension,))
 		b1 = 0.7
 		b2 = 0.95
 		steps = 3000
 		alpha = 0.01
+
+		#values for keeping track of convergence
 		oldScore = 0
 		delta = 0
 		minScore = self._ComputeScore(predictPoints)
@@ -124,58 +111,47 @@ class CLUP:
 		minl = -1
 		currentAlpha = alpha
 		alphaTrigger = 0
-		r = []
-		va = []
-		for l in range(steps):
-			diff = self.DDtInv@(self.Constraints.Vector() - self.Dpblub)
-
-			dcdz = self.Constraints.Derivative()
 		
+		for l in range(steps):
+
+			#compute gradient
+			diff = self.DDtInv@(self.Constraints.Vector() - self.Dpblub)
+			dcdz = self.Constraints.Derivative()
 			grad = 2*dcdz@diff
 
+			#compute adam step rules
 			ms = b1 * ms + (1.0 - b1) * grad
 			vs = b2 * vs + (1.0 - b2)*np.multiply(grad,grad)
-
 			c1 = 1.0/(1.0 - pow(b1,l+1))
 			c2 = 1.0/(1.0 - pow(b2,l+1))
 			step = -currentAlpha*np.divide(ms/c1, np.sqrt(vs/c2 + 1e-20))
 			self.Constraints.Update(step)
 
+
+			#compute new score
+			mse = self._ComputeScore(predictPoints)
+			if minScore == None or mse < minScore:
+				minScore = mse
+				minC = np.array(self.Constraints._OptimiseVector)
+				minl = l+1
+
+			##convergence checks
 			gNorm = np.linalg.norm(grad/len(ms))
-
-
-			s = 0
-			pp = 0
-			for i in range(len(dcdz)):
-				v1 = dcdz[i,:]
-				n1 = np.linalg.norm(v1)
-				n2 = np.linalg.norm(diff)
-				if (n1 > 1e-2 and n2 > 1e-3):
-					s += np.dot(v1,diff)/(n1*n2)
-					pp+=1
-			if (s>0):
-				r.append(l)
-				va.append(s/(pp+1e-5))
+			q= abs(mse - oldScore)/(abs(mse)+1e-7)
+			dmem = 0.9
+			delta = dmem*delta + (1.0- dmem)*q
+			oldScore = mse
+			#exit if gradient is very flat
 			if gNorm < 1e-7:
 				mse = self._ComputeScore(predictPoints)
-				print("Reached gnorm at ",l,mse,gNorm)
+				print(f"Convergence Criteria met: Gradient flat ({gNorm})")
 				break
-			if l % 1 == 0:
-				mse = self._ComputeScore(predictPoints)
-				q= abs(mse - oldScore)/(abs(mse)+1e-7)
-				dmem = 0.9
-				delta = dmem*delta + (1.0- dmem)*q
-				
-				
-				if minScore == None or mse < minScore:
-					minScore = mse
-					minC = np.array(self.Constraints._OptimiseVector)
-					minl = l+1
-				# print(f"Step {l}, score {float(mse)}, best at {minl},{minScore}, gnorm is {gNorm}, {currentAlpha}, {alphaTrigger}")
-				if (delta < 1e-10):
-					print(f"Reached stability, {delta}")
-					break
-			mse = self._ComputeScore(predictPoints)
+						
+			if (delta < 1e-10 and l > 20):
+				print(f"Convergence Criteria met: Function value stabile at, {mse}")
+				break
+
+			## some extensions to the ADAM optimiser to make it take smaller steps when large oscillations
 			if l > 5:
 				if mse > oldScore:
 					alphaTrigger += 2
@@ -186,12 +162,9 @@ class CLUP:
 					alphaTrigger = max(0,alphaTrigger-1)
 					if alphaTrigger == 0:
 						currentAlpha = min(alpha,currentAlpha*1.01)
-			oldScore = mse
-		print("Best step was found at ",minl,minScore)
-		self.Angle = [r,va]
+		
+		#set the parameter values to those which achieved the lowest score (in case of large oscillations)
 		self.Constraints._OptimiseVector[:] = minC
-		pi = []
-		corrector = self.PseudoInv@(self.Constraints.Vector() - self.Constraints.Matrix()@self.p_blups)	
 		
 	def _ComputeScore(self,predictPoints):
 		corrector = self.PseudoInv@(self.Constraints.Vector() - self.Constraints.Matrix()@self.p_blups)	
@@ -199,54 +172,50 @@ class CLUP:
 		for i in range(len(predictPoints)):
 			ai = self.a_blups[i] + corrector[i]/self.beta * self.delta
 			mse +=  ai.T @ self.K @ ai - 2 * self.ks[i].T @ai
-		return mse + self.MSE_Offset
-	
+		return mse
 
-	def ConstraintProject(self,vector):
-		Bp = self.Constraints.Matrix() @ vector
-		testPhi = Bp - self.Constraints._TotalBaseVector
+###########################################
+	#All functions below are part of the (experimental) error analysis code. They are not yet active.
+##########################################
 
-		phi = np.zeros((len(Bp),1))
-		start = 0
-		penaliser = 0
-		for i in range(len(self.Constraints._internalConstraints)):
-			dim = self.Constraints._internalConstraints[i].Dimension
-			if not self.Constraints._internalConstraints[i].IsConstant:
-				for j in range(dim):
-					if testPhi[start+j] > 0:
-						phi[start+j] = testPhi[start+j]
-					else:
-						penaliser += abs(testPhi[start+j])
+	# def ConstraintProject(self,vector):
+	# 	Bp = self.Constraints.Matrix() @ vector
+	# 	testPhi = Bp - self.Constraints._TotalBaseVector
+
+	# 	phi = np.zeros((len(Bp),1))
+	# 	start = 0
+	# 	penaliser = 0
+	# 	for i in range(len(self.Constraints._internalConstraints)):
+	# 		dim = self.Constraints._internalConstraints[i].Dimension
+	# 		if not self.Constraints._internalConstraints[i].IsConstant:
+	# 			for j in range(dim):
+	# 				if testPhi[start+j] > 0:
+	# 					phi[start+j] = testPhi[start+j]
+	# 				else:
+	# 					penaliser += abs(testPhi[start+j])
 						
 					
-			start += dim
+	# 		start += dim
 
-		bracket = phi - testPhi
-		# print(np.shape(vector),np.shape(self.Constraints.Matrix()),np.shape(bracket),np.shape(self.PseudoInv))
-		correct = vector + self.PseudoInv @(bracket)
-		# print("original",vector.T)
-		# print("transformed",correct.T)
-		return correct,penaliser*1000
+	# 	bracket = phi - testPhi
+	# 	correct = vector + self.PseudoInv @(bracket)
+	# 	return correct,penaliser*1000
 
-	def _ErrorScore(self,vector):
-		sigma = 0.05
-		ps,penalty = self.ConstraintProject(vector.reshape((len(vector),1)))
+	# def _ErrorScore(self,vector):
+	# 	sigma = 0.05
+	# 	ps,penalty = self.ConstraintProject(vector.reshape((len(vector),1)))
 		
-		score = -self.MSE_Offset
-		for i in range(len(vector)):
-			ai = self.a_blups[i] + (ps[i] - self.p_blups[i])/self.beta * self.delta
-			score -= ai.T @self.K @ai - 2*self.ks[i].T @ai
-		score -= penalty
-		# print("PS",ps.T)
-		# print("score",score)
-		# print("pen",penalty)
-		return score
-	def _Errors(self):
+	# 	score = -self.MSE_Offset
+	# 	for i in range(len(vector)):
+	# 		ai = self.a_blups[i] + (ps[i] - self.p_blups[i])/self.beta * self.delta
+	# 		score -= ai.T @self.K @ai - 2*self.ks[i].T @ai
+	# 	score -= penalty
+	# 	return score
+	# def _Errors(self):
 		vector = self.p_clups
 		
 		self.MyVec = vector.reshape((len(vector),1))
 		
-		print(self._ErrorScore(vector))
 
 		ndim = len(vector)
 		nwalkers= int(2.1*ndim)
@@ -262,7 +231,7 @@ class CLUP:
 		sampler = emcee.EnsembleSampler(nwalkers,ndim,lambda ps: self._ErrorScore(ps),moves=[
         (emcee.moves.DEMove(), 0.8),
         (emcee.moves.DESnookerMove(), 0.2),
-    ],
+    	],
 		)
 		state = sampler.run_mcmc(startPos,5000,progress=True)
 		# tau = int(np.mean(np.mean(sampler.get_autocorr_time())))
